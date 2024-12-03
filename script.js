f167eb9a6344f05172b386201407402d6e1c1878
SivuyileMtwetwe <sivuyilemtwetwe@gmail.com>
2024-12-03 14:39:08 +0200

    new material

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 
-    const eye = document.getElementById("eye");
-    const password = document.getElementById("pwd");
+    // const eye = document.getElementById("eye");
+    // const password = document.getElementById("pwd");
 
-    eye.addEventListener('click', () => {
-        const type = password.type === 'password' ? 'text' : 'password';
-        password.type = type;
+    // eye.addEventListener('click', () => {
+    //     const type = password.type === 'password' ? 'text' : 'password';
+    //     password.type = type;
 
-        if (type === 'text') {
-            eye.src = "assets/eye.svg";
-        } else {
-            eye.src = "assets/eye.svg";
-        }
-    });
+    //     if (type === 'text') {
+    //         eye.src = "assets/eye.svg";
+    //     } else {
+    //         eye.src = "assets/eye.svg";
+    //     }
+    // });
+
+
+    // Challenge #2
+
+let dropdown = document.getElementById("moviesInput")
+
+let movies = "top-100-christmas-movies.json"
+
